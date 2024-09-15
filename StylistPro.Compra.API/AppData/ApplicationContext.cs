32d33bb7a5f1514e5702784a1326e49cc377e4f0
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2024-09-15 01:00:20 -0300

    Configurando o DbContext

---
+++ StylistPro.Compra.API/AppData/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StylistPro.Compra.API.Entities;
+
+namespace StylistPro.Compra.API.AppData
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) 
+        {    
+
+        }
+
+        public DbSet<ComprasEntity> Compras { get; set; }
+    }
+}
