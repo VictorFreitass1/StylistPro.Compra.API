dd3c3c111d26f3d8113e941e262e230bf69108ca
VICTOR FREITAS SILVA <victor.fsilva45@gmail.com>
2024-09-15 00:56:19 -0300

    Add project files.

---
+++ StylistPro.Compra.API/Entities/ComprasEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StylistPro.Compra.API.Entities
+{
+    [Table("TB_COMPRAS")]
+    public class ComprasEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DataDaCompra { get; set; }
+        public string Status { get; set; }
+
+    }
+}
